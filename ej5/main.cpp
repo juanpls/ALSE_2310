8d730e528665896e4e9f9a7ec01ad8527c19906c
Alexander Pérez <alexander.perez@escuelaing.edu.co>
2023-02-17 15:26:54 -0500

    Conociendo los parámetros del main.

---
+++ ej5/main.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+
+using namespace std;
+
+float promedio( int v[], int cant ){
+  float prom = 0.;
+  
+  for( int i = 0; i < cant; i++){
+    prom += v[i];
+  }
+  
+  return prom / (float)cant;
+}
+
+float promedio( float v[], int cant ){
+  float prom = 0.;
+  
+  for( int i = 0; i < cant; i++){
+    prom += v[i];
+  }
+  
+  return prom / (float)cant;
+}
+
+
+
+int main(){
+  int vec[10];
+  float prom=0.;
+
+  // Creación de un puntero
+
+  float* dirProm;
+
+  cout << "Vector creado en dirección: " << vec << endl;
+  cout << "Dirección de v[9]: " << &vec[9] << endl;
+  
+  dirProm = &prom;
+  cout << "Dirección de prom: " << dirProm << " almacenado en: " << &dirProm << endl;
+  cout << "Ingresar diez (10) número enteros: ";
+
+  for( int i = 0; i < 10; i++){
+    cin >> vec[i];
+  }
+
+  prom = promedio( vec );
+
+  cout << "El promedio de los datos ingresados es: " << prom << "\n";
+  return 0;
+}
---
+++ ej6/main.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+
+using namespace std;
+
+float promedio( int v[], int cant ){
+  float prom = 0.;
+  
+  for( int i = 0; i < cant; i++){
+    prom += v[i];
+  }
+  
+  return prom / (float)cant;
+}
+/*
+float promedio( float v[], int cant ){
+  float prom = 0.;
+  
+  for( int i = 0; i < cant; i++){
+    prom += v[i];
+  }
+  
+  return prom / (float)cant;
+}
+*/
+
+
+int main(int argc, char** argv){
+  cout << "Cantidad de argumentos: " << argc << endl;
+
+  for(int i=0; i< argc; i++)  
+    cout << argv[i] << endl;
+
+
+  int vec[10];
+  float prom=0.;
+
+  // Creación de un puntero
+
+  float* dirProm;
+
+  cout << "Vector creado en dirección: " << vec << endl;
+  cout << "Dirección de v[9]: " << &vec[9] << endl;
+  
+  dirProm = &prom;
+  cout << "Dirección de prom: " << dirProm << " almacenado en: " << &dirProm << endl;
+  cout << "Ingresar diez (10) número enteros: ";
+
+  for( int i = 0; i < 10; i++){
+    cin >> vec[i];
+  }
+
+  prom = promedio( vec, 10 );
+
+  cout << "El promedio de los datos ingresados es: " << prom << "\n";
+  return 0;
+}
