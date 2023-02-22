022e46c1770e47cdacfb0f6314534e94242a799a
Alexander Pérez <alexander.perez@escuelaing.edu.co>
2023-02-22 16:02:11 -0500

    Incluyendo la sobrecarga de operadores.

--- eje7/main.cpp
+++ eje7/main.cpp
@@ -6,10 +6,33 @@ struct Punto{
   double x,y;
 };
 
-double distancia( Punto a, Punto b){
-  return sqrt(pow((b.y - a.y),2) + pow((b.x - a.x),2));
+struct Linea{
+  Punto a,b;
+};
+
+ostream& operator<< (ostream& out, Punto &a){
+  out << "El punto es (" << a.x << ", " << a.y << ")" << endl;
+  return out;
+}
+  
+double pendiente(const Punto &a, const Punto &b ){
+  double x = b.x - a.x;
+  double y = b.y - a.y;
+  return y / x;
 }
 
+double distancia( const Punto &a, const Punto &b ){
+  double x = b.x - a.x;
+  double y = b.y - a.y;
+  return sqrt( x * x  + y * y );
+}
+
+/*
+double longitud(Linea a){
+  double a = distancia( a.a, a.b) ;
+  return a;
+}
+*/
 int main(int argc, char** argv){
   Punto a, b;
   double dist;
@@ -19,7 +42,7 @@ int main(int argc, char** argv){
   b.y = 6.;
 
   dist = distancia(a,b);
-
+  cout << "Entre " << a << " y  " << b;
   cout << "La distancia entre ellos es: " << dist << endl;
   return 0;
 }
