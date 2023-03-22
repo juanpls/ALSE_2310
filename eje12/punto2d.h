67bea07111ea61b8d5b16d99cedc27a2f9ca68ce
Alexander Pérez <alexander.perez@escuelaing.edu.co>
2023-03-22 15:37:10 -0500

    Corrigiendo la clase Punto2D y el main que la utiliza.

--- eje12/punto2d.h
+++ eje12/punto2d.h
@@ -7,13 +7,13 @@ class Punto2D{
 
 public:
   Punto2D();
-  Punto2D( Punto2D &a );
+  Punto2D( const Punto2D &a );
   Punto2D(double r, double i );
   double   X();
   bool     X( double r );
   double   Y();
   bool     Y( double i );
-  double   distancia( const Complejo &b );
+  double   distancia( const Punto2D &b );
   double   angulo();
   double   magnitud();
   Punto2D operator+(Punto2D &a);
